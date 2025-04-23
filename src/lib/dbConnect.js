8edb982c0faf3661bfed5115157eecd81c93a46f
Your Name <you@example.com>
2025-04-23 16:35:22 +0530

    modified:   src/lib/dbConnect.js

--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -1,14 +1,18 @@
 import mongoose from "mongoose";
 
-export const connectDB = async () => {
+let isConnected = false;
+
+export async function connectDB() {
+    if (isConnected) return;
+
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
+        await mongoose.connect(process.env.MONGODB_URI, {
             dbName: "test",
         });
-
+        isConnected = true;
         console.log("MongoDB connected");
     } catch (err) {
-        console.error("MongoDB connection error:", err);
+        console.error("MongoDB connection error", err);
         throw err;
     }
-};
+}
